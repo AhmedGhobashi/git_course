71c651c4fa449512a027d164dc8b6f9d4b981b22
GHOBASHI <63506736+AhmedGhobashi@users.noreply.github.com>
2024-02-09 06:10:45 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,87 @@
+from flask import Flask, render_template, url_for
+app = Flask(__name__)
+
+# لإضافة معلومات تتغير باستمرار على ال homepage مثلا
+lessons = [{
+    'title': 'Request Library Course',
+    'course': 'Python',
+    'author': 'Omar',
+    'thumbnail': 'thumbnail.jpg'
+},
+{'title': 'Request Library Course',
+    'course': 'Python',
+    'author': 'Omar',
+    'thumbnail': 'thumbnail.jpg'
+},
+{'title': 'Request Library Course',
+    'course': 'Python',
+    'author': 'Omar',
+    'thumbnail': 'thumbnail.jpg'
+},
+{'title': 'Request Library Course',
+    'course': 'Python',
+    'author': 'Omar',
+    'thumbnail': 'thumbnail.jpg'
+},
+{'title': 'Request Library Course',
+    'course': 'Python',
+    'author': 'Omar',
+    'thumbnail': 'thumbnail.jpg'
+},
+{'title': 'Request Library Course',
+    'course': 'Python',
+    'author': 'Omar',
+    'thumbnail': 'thumbnail.jpg'
+},
+]
+
+courses = [
+{
+        'name': 'Python',
+        'icon': 'python.svg',
+        'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quidem nihil dolor officiis at magni!'
+    },
+
+    {
+        'name': 'Data Analysis',
+        'icon': 'analysis.png',
+        'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quidem nihil dolor officiis at magni!'
+    },
+
+    {
+        'name': 'Machine Learning',
+        'icon': 'machine-learning.png',
+        'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quidem nihil dolor officiis at magni!'
+    },
+
+        {
+        'name': 'Web Design',
+        'icon': 'web.png',
+        'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quidem nihil dolor officiis at magni!'
+    },
+
+        {
+        'name': 'Blockchain',
+        'icon': 'blockchain.png',
+        'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quidem nihil dolor officiis at magni!'
+    },
+
+        {
+        'name': 'Tips & Tricks',
+        'icon': 'idea.png',
+        'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quidem nihil dolor officiis at magni!'
+    },
+
+]
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html", lessons = lessons ) # نضع ال lessons ك Argument داخل ال home template  ويمكن التعامل معه من خلال ملف ال home.html
+@app.route ("/about")
+def about ():
+    return render_template("about.html")
+
+if __name__=="__main__":
+    app.run(debug=True)
